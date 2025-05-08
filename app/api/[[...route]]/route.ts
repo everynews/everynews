1b31fa6371e889d9f6ca7e30708d852f16930f2e
Sunghyun Cho <hey@cho.sh>
2025-05-08 21:56:19 +0900

    refactor: Remove unnecessary export statement

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,7 +2,6 @@ import { server } from '@everynews/server/hono'
 import { handle } from 'hono/vercel'
 
 export const dynamic = 'force-dynamic'
-export const runtime = 'edge'
 
 export const GET = handle(server)
 export const POST = handle(server)
