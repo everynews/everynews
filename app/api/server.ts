58c2ddd99034785a857ce4035160931caacdac3b
Sunghyun Cho <hey@cho.sh>
2025-06-05 04:44:07 +0900

    refactor: Remove unused code and clean up configuration file

--- app/api/server.ts
+++
@@ -1,9 +0,0 @@
-import { url } from '@everynews/lib/url'
-import type { AppType } from '@everynews/server/hono'
-import { hc } from 'hono/client'
-import { cookies } from 'next/headers'
-
-export const serverApi = async () => {
-  const cookieHeader = (await cookies()).toString()
-  return hc<AppType>(url, { init: { headers: { cookie: cookieHeader } } }).api
-}
