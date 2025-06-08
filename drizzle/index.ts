d2934ece8b3d571ec0d8dce035e271cfd9170604
Sunghyun Cho <hey@cho.sh>
2025-06-08 08:25:23 -0400

    Refactor database connection setup for drizzle module

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -1,30 +1,10 @@
 import * as schema from '@everynews/schema'
-import { type NeonQueryFunction, neon } from '@neondatabase/serverless'
-import {
-  type NeonHttpDatabase,
-  drizzle as neonDrizzle,
-} from 'drizzle-orm/neon-http'
-import { drizzle as pgDrizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
+import { drizzle } from 'drizzle-orm/neon-http'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined')
 }
 
-const dev = process.env.NODE_ENV === 'development'
-
-let db:
-  | (NeonHttpDatabase<Record<string, unknown>> & {
-      $client: NeonQueryFunction<any, any>
-    })
-  | ReturnType<typeof pgDrizzle>
-
-if (dev) {
-  db = pgDrizzle(new Pool({ connectionString: process.env.DATABASE_URL }), {
-    schema,
-  })
-} else {
-  db = neonDrizzle(neon(process.env.DATABASE_URL), { schema })
-}
-
-export { db }
+export const db = drizzle(process.env.DATABASE_URL, {
+  schema,
+})
