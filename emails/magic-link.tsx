c14cb758142f5c3eeb1dca5f246b5e23bed7c300
Sunghyun Cho <hey@cho.sh>
2025-06-08 08:39:33 -0400

    Refactor magic link email template styles

--- emails/magic-link.tsx
+++ emails/magic-link.tsx
@@ -2,8 +2,6 @@ import {
   Body,
   Button,
   Container,
-  Head,
-  Html,
   Img,
   Link,
   Preview,
@@ -14,14 +12,12 @@ import {
 
 const MagicLinkEmail = ({ loginLink }: { loginLink: string }) => {
   return (
-    <Html>
-      <Head />
       <Tailwind>
-        <Body className='my-auto bg-white px-2 font-sans'>
+        <Body className='bg-white px-2 font-sans'>
           <Preview>Sign in with magic link</Preview>
-          <Container className='my-10 max-w-md rounded border border-gray-200 p-5'>
+          <Container className='my-10 mx-auto rounded border border-gray-200 p-5 text-center'>
             <Link href={loginLink} className='text-blue-600 no-underline'>
-              <Section className='my-8'>
+              <Section className='my-8 flex justify-center'>
                 <Img
                   src='https://every.news/logo.png'
                   width='128'
@@ -29,21 +25,21 @@ const MagicLinkEmail = ({ loginLink }: { loginLink: string }) => {
                   alt='every.news Logo'
                 />
               </Section>
-              <Button
-                className='rounded bg-black px-4 py-2 text-lg font-semibold text-white'
-                href={loginLink}
-              >
-                Sign in to every.news
-              </Button>
+              <div className='flex justify-center'>
+                <Button
+                  className='mx-auto rounded bg-black px-4 py-2 text-lg font-semibold text-white'
+                  href={loginLink}
+                >
+                  Sign in to every.news
+                </Button>
+              </div>
             </Link>
-
-            <Text className='text-sm'>
+            <Text className='text-sm text-center'>
               If you did not request this, please ignore this email.
             </Text>
           </Container>
         </Body>
       </Tailwind>
-    </Html>
   )
 }
 
