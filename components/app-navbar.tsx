bcf7a3797e74e28943d36aac70cca22b72a5a8a5
Sunghyun Cho <anaclumos@gmail.com>
2025-06-14 14:20:49 +0900

    feat: add mobile navbar

--- components/app-navbar.tsx
+++ components/app-navbar.tsx
@@ -2,6 +2,7 @@ import { whoami } from '@everynews/auth/session'
 import { NavUser } from '@everynews/components/auth/nav-user'
 import { SignIn } from '@everynews/components/auth/sign-in'
 import { Logo } from '@everynews/public/logo'
+import { MobileNav } from './mobile-nav'
 import Link from 'next/link'
 import { Suspense } from 'react'
 import { ThemeToggle } from './theme/toggle'
@@ -10,15 +11,16 @@ export const AppNavbar = async () => {
   const user = await whoami()
 
   return (
-    <div className='flex gap-6 md:gap-10 mx-auto container p-4 justify-between items-center'>
-      <div className='flex gap-6'>
-        <Link href='/' className='flex items-center gap-2'>
-          <Logo />
-          <span className='inline-block font-bold'>Everynews</span>
-          <span className='inline-block px-1.5 py-0.5 text-xs font-medium bg-blue-100 text-blue-800 dark:bg-blue-900/30 dark:text-blue-300 rounded-md'>
-            beta
-          </span>
-        </Link>
+    <div className='mx-auto container flex items-center justify-between gap-6 p-4 md:gap-10'>
+      <Link href='/' className='flex items-center gap-2'>
+        <Logo />
+        <span className='inline-block font-bold'>Everynews</span>
+        <span className='inline-block rounded-md bg-blue-100 px-1.5 py-0.5 text-xs font-medium text-blue-800 dark:bg-blue-900/30 dark:text-blue-300'>
+          beta
+        </span>
+      </Link>
+      <MobileNav user={user} />
+      <div className='hidden items-center gap-2 md:flex'>
         <nav className='flex gap-6'>
           <Link
             href='/newsletters'
@@ -27,13 +29,9 @@ export const AppNavbar = async () => {
             Newsletters
           </Link>
         </nav>
-      </div>
-      <div className='flex items-center gap-2'>
         <ThemeToggle />
         <Suspense
-          fallback={
-            <div className='h-8 w-8 animate-pulse rounded-full bg-muted' />
-          }
+          fallback={<div className='h-8 w-8 animate-pulse rounded-full bg-muted' />}
         >
           {user ? <NavUser user={user} /> : <SignIn />}
         </Suspense>
