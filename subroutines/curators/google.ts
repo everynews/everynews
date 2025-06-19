e5d4bc7c88cdc276e051cd86acecef6f68a839e7
Sunghyun Cho <hey@cho.sh>
2025-06-20 01:13:40 +0900

    Update Google curator to include news search filter in query URL

--- subroutines/curators/google.ts
+++ subroutines/curators/google.ts
@@ -21,7 +21,7 @@ export const GoogleCurator: Curator = async (
   const response = await fetch('https://api.brightdata.com/request', {
     body: JSON.stringify({
       format: 'raw',
-      url: `https://www.google.com/search?q=${encodeURIComponent(alert.strategy.query)}`,
+      url: `https://www.google.com/search?q=${encodeURIComponent(alert.strategy.query)}&tbm=nws`,
       zone: 'everynews_serp',
     }),
     headers: {
