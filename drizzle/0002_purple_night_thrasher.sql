6f1d783d538b4b85cb47eec0d38b878acdd93fc2
Sunghyun Cho <hey@cho.sh>
2025-07-13 22:46:59 +0900

    feat: Add verifications table and corresponding schema for improved data handling and integrity

---
+++ drizzle/0002_purple_night_thrasher.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "verifications" (
+	"created_at" timestamp,
+	"expires_at" timestamp NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
+	"identifier" text NOT NULL,
+	"updated_at" timestamp,
+	"value" text NOT NULL
+);
