589c6476522496d5b710a7aeb26badb8bac61cb3
Sunghyun Cho <hey@cho.sh>
2025-07-13 22:49:46 +0900

    feat: Add accounts table and corresponding schema for user authentication, including foreign key constraints and metadata for improved data integrity

---
+++ drizzle/0003_secret_frank_castle.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "accounts" (
+	"access_token" text,
+	"access_token_expires_at" timestamp,
+	"account_id" text NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
+	"id_token" text,
+	"password" text,
+	"provider_id" text NOT NULL,
+	"refresh_token" text,
+	"refresh_token_expires_at" timestamp,
+	"scope" text,
+	"updated_at" timestamp NOT NULL,
+	"user_id" text NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
---
+++ schema/account.ts
@@ -0,0 +1,20 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { users } from './user'
+
+export const accounts = pgTable('accounts', {
+  accessToken: text('access_token'),
+  accessTokenExpiresAt: timestamp('access_token_expires_at'),
+  accountId: text('account_id').notNull(),
+  createdAt: timestamp('created_at').notNull(),
+  id: text('id').primaryKey(),
+  idToken: text('id_token'),
+  password: text('password'),
+  providerId: text('provider_id').notNull(),
+  refreshToken: text('refresh_token'),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+  scope: text('scope'),
+  updatedAt: timestamp('updated_at').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+})
